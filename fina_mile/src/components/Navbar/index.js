6fa2c6f3423c80a36a2093d5f1522d6426707f6f
ZhuoEdwardWang <wangzhuo2001@gmail.com>
2022-11-20 12:48:54 -0500

    proxy working

---
+++ fina_mile/src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {Nav, NavLink, NavMenu} from './NavbarElements';
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/map" activeStyle>
+            Map
+          </NavLink>
+          <NavLink to="/contact" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/blogs" activeStyle>
+            Blogs
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
