efdf1c2e6ff8f24bc35996b18f86aa92e4933881
SunJames568 <sunjames568@gmail.com>
2022-12-04 11:52:19 -0500

    updated table creation files

--- fina_mile/finamile.sql
+++ fina_mile/finamile.sql
@@ -1,17 +1,31 @@
-CREATE TABLE users(
-	userId INT PRIMARY KEY,
-	userName VARCHAR(256),
+CREATE TABLE user(
+	name VARCHAR(256),
 	email VARCHAR(256),
 	pwd VARCHAR(256),
-	address VARCHAR(256)
+	building_number varchar(50),
+	street varchar(50),
+	city varchar(50),
+	state varchar(50),
+	PRIMARY KEY (email)	
+);
+
+CREATE TABLE user_package(
+	email VARCHAR(256),
+	packageId INT,
+	status VARCHAR(50),
+	requestTime DATETIME,
+	PRIMARY KEY (email, packageId),	
+	FOREIGN KEY (userId)
+		REFERENCES users (userId)
+	FOREIGN KEY (packageId)
+		REFERENCES package (packageId)
 );
 
 CREATE TABLE package(
-	userId INT NOT NULL,
-	pkgId INT NOT NULL,
-	origin VARCHAR(256),
-	status VARCHAR(50),
-	PRIMARY KEY (userId, pkgId),	
-	FOREIGN KEY (userId)
-		REFERENCES users (userId)
+	packageId INT NOT NULL,
+	building_number varchar(50),
+	street varchar(50),
+	city varchar(50),
+	state varchar(50),
+	PRIMARY KEY (packageId)
 );
