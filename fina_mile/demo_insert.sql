3aa04f67f47f93dadc9346493941c3222d87622b
Zedward <60852119+ZhuoEdwardWang@users.noreply.github.com>
2022-12-07 09:22:19 -0500

    Dec 7th

---
+++ fina_mile/demo_insert.sql
@@ -0,0 +1,16 @@
+insert into users values('testcustomer@nyu.edu', 'Test Customer 1', '81dc9bdb52d04dc20036dbd8313ed055', '1555', 'Jay St', 'Brooklyn', 'New York');
+insert into users values('user1@nyu.edu', 'User 1', '81dc9bdb52d04dc20036dbd8313ed055', '5405', 'Jay Street', 'Brooklyn', 'New York');
+insert into users values('user2@nyu.edu', 'User 2', '81dc9bdb52d04dc20036dbd8313ed055', '1702', 'Jay Street', 'Brooklyn', 'New York');
+insert into users values('user3@nyu.edu', 'User 3', '81dc9bdb52d04dc20036dbd8313ed055', '1890', 'Jay Street', 'Brooklyn', 'New York');
+
+insert into package values(25, '311', 'Jay St', 'Brooklyn', 'New York');
+insert into package values(24, '251', 'Palm St', 'Miami', 'Florida');
+insert into package values(29, '251', '14 St', 'Manhattan', 'New York');
+
+insert into users_package values('testcustomer@nyu.edu', 25, 'time', '2022-06-12 13:25:25');
+insert into users_package values('testcustomer@nyu.edu', 24, 'time', '2022-07-12 13:25:25');
+
+insert into users_package values('user1@nyu.edu', 25, 'time', '2022-06-10 13:25:25');
+insert into users_package values('user1@nyu.edu', 29, 'money', '2022-06-12 13:25:25');
+
+
--- fina_mile/finamile.sql
+++ fina_mile/finamile.sql
@@ -1,17 +1,31 @@
 CREATE TABLE users(
-	userId INT PRIMARY KEY,
-	userName VARCHAR(256),
 	email VARCHAR(256),
+	name VARCHAR(256),
 	pwd VARCHAR(256),
-	address VARCHAR(256)
+	building_number varchar(50),
+	street varchar(50),
+	city varchar(50),
+	state varchar(50),
+	PRIMARY KEY (email)	
 );
 
 CREATE TABLE package(
-	userId INT NOT NULL,
-	pkgId INT NOT NULL,
-	origin VARCHAR(256),
-	status VARCHAR(50),
-	PRIMARY KEY (userId, pkgId),	
-	FOREIGN KEY (userId)
-		REFERENCES users (userId)
+	packageId INT NOT NULL,
+	building_number varchar(50),
+	street varchar(50),
+	city varchar(50),
+	state varchar(50),
+	PRIMARY KEY (packageId)
+);
+
+CREATE TABLE users_package(
+	email VARCHAR(256),
+	packageId INT,
+	preference VARCHAR(256),
+	requestTime TIMESTAMP,
+	PRIMARY KEY (email, packageId),	
+	FOREIGN KEY (email)
+		REFERENCES users (email),
+	FOREIGN KEY (packageId)
+		REFERENCES package (packageId)
 );
